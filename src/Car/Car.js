44d051ed320898a05ecbed56e915e52f9892493f
oabanin-WORK <juristaba@gmail.com>
2020-06-03 17:32:19 +0400

    react components dynamic styles

---
+++ src/Car/Car.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Car.css'
+
+export default props => {
+  const inputClasses = ['input']
+
+  if (props.name !== '') {
+    inputClasses.push('green')
+  } else {
+    inputClasses.push('red')
+  }
+
+  if (props.name.length > 4) {
+    inputClasses.push('bold')
+  }
+
+  return (
+    <div className="Car">
+      <h3>Сar name: {props.name}</h3>
+      <p>Year: <strong>{props.year}</strong></p>
+      <input
+        type="text"
+        onChange={props.onChangeName}
+        value={props.name}
+        className={inputClasses.join(' ')}
+      />
+      <button onClick={props.onDelete}>Delete</button>
+    </div>
+  )
+}
